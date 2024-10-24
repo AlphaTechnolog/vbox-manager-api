02e136685eaa0efffdec1dae43091af7f877bfd8
Gabriel Guerra <gfranklings@gmail.com>
2024-10-24 19:59:56 -0400

    nix: update Zig cache hash

--- nix/zigCacheHash.nix
+++ nix/zigCacheHash.nix
@@ -1,3 +1,3 @@
 # This file is auto-generated! check build-support/check-zig-cache-hash.sh for
 # more details.
-"sha256-o1ILwxLWf0jB8X9A35pgQmTds4oWwt0WBoblHqbOXkY="
+"sha256-n88yCIFJcz/vZs6wWSLx0KMvdMfcuH+pFO6ReBBUvJc="
